ffc465ed759d81830940c417c214a4ffa660a595
Vincent De Borger <vincent.deborger@gmail.com>
2025-10-12 16:09:56 +0200

    fix: typo

--- internal/modrinth/file.go
+++ internal/modrinth/file.go
@@ -10,24 +10,24 @@ import (
 	"os"
 )
 
-// Get the Sha256 has of a file
+// Get the Sha512 hash of a file
 func getHash(path string) (string, error) {
-  file, err := os.Open(path)
-  if err != nil {
-    return "", err
-  }
-  defer file.Close()
+	file, err := os.Open(path)
+	if err != nil {
+		return "", err
+	}
+	defer file.Close()
 
-  // Create a new hash interfact
-  hash := sha512.New()
+	// Create a new hash interfact
+	hash := sha512.New()
 
-  // Copy data from file into hash function
-  if _, err = io.Copy(hash, file); err != nil {
-    return "", err
-  } 
+	// Copy data from file into hash function
+	if _, err = io.Copy(hash, file); err != nil {
+		return "", err
+	}
 
-  // Get resulting hash 
-  hashBytes := hash.Sum(nil)
+	// Get resulting hash
+	hashBytes := hash.Sum(nil)
 
-  return fmt.Sprintf("%x", hashBytes), nil
+	return fmt.Sprintf("%x", hashBytes), nil
 }
