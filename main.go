6844ec6b8d7c2af352c7742649a4dea518482358
Vincent De Borger <vincent.deborger@gmail.com>
2025-10-12 14:52:45 +0200

    feat: create release using github workflow

--- main.go
+++ main.go
@@ -1,10 +1,21 @@
 /*
-  Copyright © 2025 Vincent De Borger <hello@vincentdeborger.be>
+Copyright © 2025 Vincent De Borger <hello@vincentdeborger.be>
 */
 package main
 
-import "github.com/DB-Vincent/mc-modpack/cmd"
+import (
+	"fmt"
+	"os"
+
+	"github.com/DB-Vincent/mc-modpack/cmd"
+)
+
+var version = "dev" // This will be overridden by ldflags during build
 
 func main() {
+	if len(os.Args) > 1 && os.Args[1] == "version" {
+		fmt.Println(version)
+		return
+	}
 	cmd.Execute()
 }
