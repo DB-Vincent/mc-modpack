4a4a5212734162a2df9e83f95e2c76aef01755bf
Vincent De Borger <vincent.deborger@gmail.com>
2025-10-12 15:55:21 +0200

    fix: remove redundant argument validation

--- cmd/add.go
+++ cmd/add.go
@@ -4,7 +4,7 @@ Copyright © 2025 Vincent De Borger <hello@vincentdeborger.be>
 package cmd
 
 import (
-  "fmt"
+	"fmt"
 	"os"
 
 	"github.com/DB-Vincent/mc-modpack/internal/config"
@@ -15,61 +15,58 @@ import (
 var addCmd = &cobra.Command{
 	Use:   "add [mod-name]",
 	Short: "Adds a mod to the current modpack",
-  Args:  cobra.MatchAll(cobra.ExactArgs(1), cobra.OnlyValidArgs),
+	Args:  cobra.MatchAll(cobra.ExactArgs(1), cobra.OnlyValidArgs),
 	Run: func(cmd *cobra.Command, args []string) {
-    if (len(args) != 1) {
-      cmd.Usage()
-    }
-    modName := args[0]
+		modName := args[0]
 
-    path := workingDirectory
-    if workingDirectory == "" {
-      var err error
-      path, err = os.Getwd()
-      if err != nil {
-        log.Error(fmt.Sprintf("Failed to get current working directory: %s", err.Error()))
-      }
-    }
+		path := workingDirectory
+		if workingDirectory == "" {
+			var err error
+			path, err = os.Getwd()
+			if err != nil {
+				log.Error(fmt.Sprintf("Failed to get current working directory: %s", err.Error()))
+			}
+		}
 
-    if err := config.Exists(path); err != nil {
-      log.Error(fmt.Sprintf("Failed to check if config exists: %s", err.Error()))
-    } 
+		if err := config.Exists(path); err != nil {
+			log.Error(fmt.Sprintf("Failed to check if config exists: %s", err.Error()))
+		}
 
-    cfg, err := config.Load(path)
-    if err != nil {
-      log.Error(fmt.Sprintf("Failed to load configuration file: %s", err.Error()))
-    }
+		cfg, err := config.Load(path)
+		if err != nil {
+			log.Error(fmt.Sprintf("Failed to load configuration file: %s", err.Error()))
+		}
 
-    version, err := modrinth.GetLatestVersion(modName, cfg.McVersion, cfg.Loader)
-    if err != nil {
-      log.Error(fmt.Sprintf("Failed retrieving latest version of specified mod: %s", err.Error()))
-    }
+		version, err := modrinth.GetLatestVersion(modName, cfg.McVersion, cfg.Loader)
+		if err != nil {
+			log.Error(fmt.Sprintf("Failed retrieving latest version of specified mod: %s", err.Error()))
+		}
 
-    // Check if mod exists in config file
-    modExists, index := config.HasMod(*cfg, modName)
-    
-    if modExists {
-      log.Info(fmt.Sprintf("Current version: %s, updating to new version: %s", cfg.Mods[index].Version, version.ModVersion))
-      cfg.Mods[index].Version = version.ModVersion
-      cfg.Mods[index].VersionId = version.VersionId
-    } else {
-      log.Info(fmt.Sprintf("Adding %s with version %s", modName, version.ModVersion))
-      cfg.Mods = append(cfg.Mods, config.Mod{
-        Name: modName,
-        VersionId: version.VersionId, 
-        Version: version.ModVersion,
-      })
-    }
+		// Check if mod exists in config file
+		modExists, index := config.HasMod(*cfg, modName)
 
-    if err = modrinth.DownloadFile(path, version.Files[0]); err != nil {
-      log.Error(fmt.Sprintf("Failed to download mod file: %s", err.Error()))
-    }
-    log.Info(fmt.Sprintf("Downloaded mod file %s", version.Files[0].Name))
+		if modExists {
+			log.Info(fmt.Sprintf("Current version: %s, updating to new version: %s", cfg.Mods[index].Version, version.ModVersion))
+			cfg.Mods[index].Version = version.ModVersion
+			cfg.Mods[index].VersionId = version.VersionId
+		} else {
+			log.Info(fmt.Sprintf("Adding %s with version %s", modName, version.ModVersion))
+			cfg.Mods = append(cfg.Mods, config.Mod{
+				Name:      modName,
+				VersionId: version.VersionId,
+				Version:   version.ModVersion,
+			})
+		}
 
-    err = config.Update(path, *cfg)
-    if err != nil {
-      log.Error(fmt.Sprintf("Failed updating the configuration file: %s", err.Error()))
-    }
+		if err = modrinth.DownloadFile(path, version.Files[0]); err != nil {
+			log.Error(fmt.Sprintf("Failed to download mod file: %s", err.Error()))
+		}
+		log.Info(fmt.Sprintf("Downloaded mod file %s", version.Files[0].Name))
+
+		err = config.Update(path, *cfg)
+		if err != nil {
+			log.Error(fmt.Sprintf("Failed updating the configuration file: %s", err.Error()))
+		}
 	},
 }
 
--- cmd/del.go
+++ cmd/del.go
@@ -5,7 +5,7 @@ package cmd
 
 import (
 	"fmt"
-  "os"
+	"os"
 
 	"github.com/DB-Vincent/mc-modpack/internal/config"
 	"github.com/DB-Vincent/mc-modpack/internal/modrinth"
@@ -15,52 +15,49 @@ import (
 var delCmd = &cobra.Command{
 	Use:   "del",
 	Short: "Removes a mod from the current modpack",
-  Args:  cobra.MatchAll(cobra.ExactArgs(1), cobra.OnlyValidArgs),
+	Args:  cobra.MatchAll(cobra.ExactArgs(1), cobra.OnlyValidArgs),
 	Run: func(cmd *cobra.Command, args []string) {
-		if (len(args) != 1) {
-      cmd.Usage()
-    }
-    modName := args[0]
+		modName := args[0]
 
-    path := workingDirectory
-    if workingDirectory == "" {
-      var err error
-      path, err = os.Getwd()
-      if err != nil {
-        log.Error(fmt.Sprintf("Failed to get current working directory: %s", err.Error()))
-      }
-    }
+		path := workingDirectory
+		if workingDirectory == "" {
+			var err error
+			path, err = os.Getwd()
+			if err != nil {
+				log.Error(fmt.Sprintf("Failed to get current working directory: %s", err.Error()))
+			}
+		}
 
-    if err := config.Exists(path); err != nil {
-      log.Error(fmt.Sprintf("Failed to check if config exists: %s", err.Error()))
-    } 
+		if err := config.Exists(path); err != nil {
+			log.Error(fmt.Sprintf("Failed to check if config exists: %s", err.Error()))
+		}
 
-    cfg, err := config.Load(path)
-    if err != nil {
-      log.Error(fmt.Sprintf("Failed to load configuration file: %s", err.Error()))
-    }
+		cfg, err := config.Load(path)
+		if err != nil {
+			log.Error(fmt.Sprintf("Failed to load configuration file: %s", err.Error()))
+		}
 
-    // Check if mod exists in config file
-    modExists, index := config.HasMod(*cfg, modName)
+		// Check if mod exists in config file
+		modExists, index := config.HasMod(*cfg, modName)
 
-    if modExists {
-      mod, err := modrinth.GetSpecificVersion(modName, cfg.Mods[index].VersionId)
-      if err != nil {
-        log.Error(fmt.Sprintf("Failed to get information for requested mod: %s", err.Error()))
-      }
+		if modExists {
+			mod, err := modrinth.GetSpecificVersion(modName, cfg.Mods[index].VersionId)
+			if err != nil {
+				log.Error(fmt.Sprintf("Failed to get information for requested mod: %s", err.Error()))
+			}
 
-      cfg.Mods = append(cfg.Mods[:index], cfg.Mods[index+1:]...)
-      config.Update(path, *cfg)
-      log.Info(fmt.Sprintf("Removed %s from list of mods", modName))
-      
-      // Remove jar file of deleted mod 
-      if err = os.Remove(fmt.Sprintf("%s/%s", path, mod.Files[0].Name)); err != nil {
-        log.Error(fmt.Sprintf("Failed removing the mod's jar-file: %s", err.Error()))
-      }
-      log.Info(fmt.Sprintf("Deleted %s", mod.Files[0].Name))
-    } else {
-      log.Info(fmt.Sprintf("Mod with name %s does not exist in configuration file", modName))
-    }
+			cfg.Mods = append(cfg.Mods[:index], cfg.Mods[index+1:]...)
+			config.Update(path, *cfg)
+			log.Info(fmt.Sprintf("Removed %s from list of mods", modName))
+
+			// Remove jar file of deleted mod
+			if err = os.Remove(fmt.Sprintf("%s/%s", path, mod.Files[0].Name)); err != nil {
+				log.Error(fmt.Sprintf("Failed removing the mod's jar-file: %s", err.Error()))
+			}
+			log.Info(fmt.Sprintf("Deleted %s", mod.Files[0].Name))
+		} else {
+			log.Info(fmt.Sprintf("Mod with name %s does not exist in configuration file", modName))
+		}
 	},
 }
 
